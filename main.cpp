09650b32cd000747ecd546477f5929709a81d0c9
nivcoo <nico64800@live.fr>
2020-11-18 21:02:46 +0100

    improv. edit QT

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
-#include "manager/window/WindowManager.h"
+#include "window/main/MainWindow.h"
 
 #include <QApplication>
+#include <QFile>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    WindowManager w;
+    QCoreApplication::setOrganizationName("CESI");
+    QCoreApplication::setApplicationName("CESI");
+    QFile File(":global.qss");
+    if (File.open(QFile::ReadOnly)) {
+        QString StyleSheet = QLatin1String(File.readAll());
+        a.setStyleSheet(StyleSheet);
+
+    }
+    MainWindow w;
+
     w.show();
+
     return a.exec();
 }
