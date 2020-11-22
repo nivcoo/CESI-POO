dc21be71e6371b3358e2d1b2731b85ddcc8f269b
antoine <contact@ant0ine64.fr>
2020-11-22 12:24:01 +0100

    Add OrderItemModel

--- model/order/OrderItemModel.cpp
+++ model/order/OrderItemModel.cpp
@@ -3,3 +3,25 @@
 //
 
 #include "OrderItemModel.h"
+#include "../../ihm/IHM.h"
+
+void OrderItemModel::insert(string orderReference, string itemReference, int quantity, double price, double vat) {
+    SACommand cmd;
+    cmd.setCommandText("INSERT INTO `order__item` VALUES (:1, :2, :3, :4, :5);");
+    cmd.Param(1).setAsString() = _TSA(orderReference).c_str();
+    cmd.Param(1).setAsString() = _TSA(itemReference).c_str();
+    cmd.Param(4).setAsInt64() = _TSA(quantity);
+    cmd.Param(7).setAsDouble() = _TSA(price);
+    cmd.Param(7).setAsDouble() = _TSA(vat);
+    ModelManager::sendCMD(&cmd);
+
+}
+
+void OrderItemModel::deleteById(string orderReference, string itemReference) {
+    SACommand cmd;
+    cmd.setCommandText("DELETE FROM `order__item` WHERE `reference` = :1 AND `reference_item` = :2;");
+    cmd.Param(1).setAsString() = _TSA(orderReference).c_str();
+    cmd.Param(1).setAsString() = _TSA(itemReference).c_str();
+    ModelManager::sendCMD(&cmd);
+
+}
--- model/order/OrderItemModel.h
+++ model/order/OrderItemModel.h
@@ -6,7 +6,13 @@
 #define MAIN_CPP_ORDERITEMMODEL_H
 
 
+#include "../../utils/DataBase.h"
+
 class OrderItemModel {
+public:
+    void insert(string orderReference, string itemReference, int quantity, double price, double vat);
+
+    void deleteById(string orderReference, string itemReference);
 
 };
 
