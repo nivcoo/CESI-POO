227a7bd6a9d83e264dfa012f2b2692fa4840f22f
nivcoo <nico64800@live.fr>
2020-11-25 09:41:55 +0100

    feat. add stats model

---
+++ model/StatModel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nicolas on 25/11/2020.
+//
+
+#include "StatModel.h"
---
+++ model/StatModel.h
@@ -0,0 +1,14 @@
+//
+// Created by Nicolas on 25/11/2020.
+//
+
+#ifndef GLOBAL_QSS_STATMODEL_H
+#define GLOBAL_QSS_STATMODEL_H
+
+
+class StatModel {
+
+};
+
+
+#endif //GLOBAL_QSS_STATMODEL_H
