6d489147bf63353c40eb1bb7d2705683ee1e100c
nivcoo <nico64800@live.fr>
2020-11-29 14:53:27 +0100

    feat. remove useless line

--- model/ModelManager.cpp
+++ model/ModelManager.cpp
@@ -63,18 +63,11 @@ void ModelManager::initDB() {
 
     QString allSQL = QLatin1String(sql.readAll());
     SAString allSQLStr = SAString(allSQL.toLatin1());
-    DataBase *db = getDataBase();
     SACommand cmd;
     cmd.setCommandText(allSQLStr);
-    db->connectAndExecuteCommand(&cmd);
-    db->closeConnection();
+    _db->connectAndExecuteCommand(&cmd);
+    _db->closeConnection();
     sql.close();
-    /**test->addAddressToCustomerID(customerID, 1, "41 rue", "64800", "okkk");
-    test->addAddressToCustomerID(customerID, 2, "78 rue", "64000", "uyk");**/
-    //test->addCustomer();
-    //_customerAddressModel->insert(2,1);
-    //_addressModel->insert(1, "41 rue de lassun", "64800", "Montaut", false);
-    //_customerModel->deleteCustomerByID(1);
 }
 
 void ModelManager::sendCMD(SACommand *cmd) {
