181a4a7596cb3faa74046be1b7ef89ceffa5de94
nivcoo <nico64800@live.fr>
2020-12-01 11:14:54 +0100

    improv. main datas

---
+++ assets/database/insertData.sql
@@ -0,0 +1,82 @@
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (1, 'Nicolin', 'Duvis', '2017-09-12', 1, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (2, 'Tonito', 'De Jueba', '2015-02-26', 2, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (3, 'Patricia ', 'Richard', '2020-04-05', 3, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (4, 'Yannis', 'Monnaie', '2019-08-06', 6, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (5, 'Jean-Damien', 'Roccard', '2012-11-09', 9, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (6, 'Dominique', 'Passandre', '2017-07-26', 12, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (7, 'Michelle', 'Berroy', '2014-09-06', 15, 1);
+INSERT INTO poo.staff (id, firstname, lastname, hire_date, id_address, id_staff) VALUES (8, 'Séraphinne', 'Vatine', '2018-05-09', 16, 1);
+
+
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (1, 0, '42 Chemin des champigons', '64320', 'Ousse', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (2, 0, '14 Place du marché', '32450', 'Soubiès-sur-lac', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (3, 0, '98 Avenue Lagrande', '65240', 'Petresnac', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (4, 1, '50 rue de fevrier', '64800', 'Igon', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (5, 2, '50 rue de fevrier', '64800', 'Igon', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (6, 0, '46 rue du 8 mai', '64300', 'Mouxer', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (7, 1, '41 rue jobin', '13003', 'Marseille', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (8, 2, '41 rue jobin', '13003', 'Marseille', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (9, 0, '6 place des oliviers', '64570', 'Iègue-en-bourg-sur-Nive', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (10, 1, '23 avenue Jean Portalis', '93290', 'Tremblay en France', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (11, 2, '23 avenue Jean Portalis', '93290', 'Tremblay en France', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (12, 0, '75 avenue Jean Moulin', '64000', 'Pau', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (13, 1, '2 routes de Lyon', '13800', 'Istres', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (14, 2, '2 routes de Lyon', '13800', 'Istres', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (15, 0, '5 chemin du maïs', '64320', 'Sendets', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (16, 0, '12 place de l''église', '64810', 'Lembeye-de-haut', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (17, 1, '11 rue marie de médicis', '69300', 'Caluire et cuire', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (18, 2, '11 rue marie de médicis', '69300', 'Caluire et cuire', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (19, 1, '82 boulevard albin durand', '51000', 'Chalon en champagne', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (20, 2, '82 boulevard albin durand', '51000', 'Chalon en champagne', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (21, 1, '7 rue de raymond poincaré', '92200', 'Neuilly sur seine', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (22, 2, '7 rue de raymond poincaré', '92200', 'Neuilly sur seine', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (23, 1, '2 Chemin du petit nain', '64160', 'Morlàas', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (24, 2, '2 Chemin du petit nain', '64160', 'Morlàas', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (25, 1, '26  rue des partolles', '64320', 'Lée', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (26, 2, '26 rue des partolles', '64320', 'Lée', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (27, 1, '4 Place de la monnaie', '64000', 'Pau', 0);
+INSERT INTO poo.address (id, type, address_line, postal_code, city, archived) VALUES (28, 2, '4 Place de la monnaie', '64000', 'Pau', 0);
+
+
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (2, 'Olivier', 'Ornologue', '1965-01-10', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (3, 'Didier', 'Gers', '1980-05-24', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (4, 'Cloridan', 'BeauChamps', '1950-07-22', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (5, 'Nicole', 'Lapresse', '1969-10-01', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (6, 'Benjamin', 'Delmar', '1967-09-24', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (7, 'Marie-Antoinette', 'Laborde', '1975-06-19', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (8, 'Quincy', 'Cuillerier', '2001-03-25', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (9, 'Nathan', 'Saliot', '1999-03-17', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (10, 'Antoine', 'Vachon', '1998-08-15', 0);
+INSERT INTO poo.customer (id, firstname, lastname, birth_date, archived) VALUES (11, 'Ethan', 'Vicoy', '1995-12-08', 0);
+
+
+INSERT INTO poo.customer_address (id, id_address) VALUES (2, 4);
+INSERT INTO poo.customer_address (id, id_address) VALUES (2, 5);
+INSERT INTO poo.customer_address (id, id_address) VALUES (3, 7);
+INSERT INTO poo.customer_address (id, id_address) VALUES (3, 8);
+INSERT INTO poo.customer_address (id, id_address) VALUES (4, 10);
+INSERT INTO poo.customer_address (id, id_address) VALUES (4, 11);
+INSERT INTO poo.customer_address (id, id_address) VALUES (5, 13);
+INSERT INTO poo.customer_address (id, id_address) VALUES (5, 14);
+INSERT INTO poo.customer_address (id, id_address) VALUES (6, 17);
+INSERT INTO poo.customer_address (id, id_address) VALUES (6, 18);
+INSERT INTO poo.customer_address (id, id_address) VALUES (7, 27);
+INSERT INTO poo.customer_address (id, id_address) VALUES (7, 28);
+INSERT INTO poo.customer_address (id, id_address) VALUES (8, 19);
+INSERT INTO poo.customer_address (id, id_address) VALUES (8, 20);
+INSERT INTO poo.customer_address (id, id_address) VALUES (9, 25);
+INSERT INTO poo.customer_address (id, id_address) VALUES (9, 26);
+INSERT INTO poo.customer_address (id, id_address) VALUES (10, 21);
+INSERT INTO poo.customer_address (id, id_address) VALUES (10, 22);
+INSERT INTO poo.customer_address (id, id_address) VALUES (11, 23);
+INSERT INTO poo.customer_address (id, id_address) VALUES (11, 24);
+
+
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('CF35800VF', 'Condensateur 800nf 35V', 30, 470, 0.06, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('CF60740VW', 'Condensateur 740nf 60V', 25, 472, 0.23, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('DI35128UI', 'Diode Xener 3A', 60, 315, 0.6, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('DI87321OI', 'Diode 5A/300V', 20, 150, 2.14, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('RS00470UU', 'Résistance 470 ohm', 20, 500, 0.03, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('RS53000FU', 'Résistance 53kOhm', 50, 645, 0.03, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('TR78155AC', 'Transistor NPN', 40, 600, 0.5, 0.2, 0);
+INSERT INTO poo.item (reference, name, resuply_threshold, quantity, price_ht, vat, archived) VALUES ('TR78595GC', 'Transistor PNP', 30, 250, 0.42, 0.2, 0);
